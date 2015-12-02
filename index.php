fcec84ee9a5f38bfd04597c68f8a38e39e7709f9
Michael <mbs9b7@mail.missouri.edu>
2015-12-01 20:25:02 -0600

    got the google map hooked up

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <html>
 	<head>
-
+		 <style type="text/css">
+      		html, body { height: 100%; margin: 0; padding: 0; }
+     		 #map { height: 75%; }
+   		</style>
+    	 
 	</head>
 	<body>
 		Please select the average temperature you are looking for:
 		<form action="<?=$_SERVER['PHP_SELF'] ?>" method="POST">
 		<select name="temper">
 			<option value="60"> 60 </option>
-			<option value="65" selected> 65 </option>
+			<option value="65"> 65 </option>
 			<option value="70"> 70 </option>
 			<option value="75"> 75 </option>
 			<option value="80"> 80 </option>
@@ -23,7 +27,7 @@
 			} 
 			if(isset($_POST['Execute'])==TRUE)
 			{
-				$sql = "SELECT * FROM test2";
+				$sql = "SELECT * FROM test3";
 				$result = $conn->query($sql);
 				$chosenTemp = $_POST['temper'];
 
@@ -32,9 +36,22 @@
 				    while($row = $result->fetch_assoc()) {
 				        if($row["id"] == $chosenTemp)
 				        {
-				        	echo "Longitude: " . $row["lon"] . "<br>Latitude: " . $row["lat"] . "<br>";
+				        	$lat = $row["lat"];
+				        	$lon = $row["lon"];
+				        	
+				        	echo "<script>
+								var map;
+								function initMap() {
+  								map = new google.maps.Map(document.getElementById('map'), {
+    							center: {lat: ".$lat.", lng: ".$lon."},
+  				  				zoom: 8
+  								});
+								}
+						    	</script>";
+				        	echo '<script async defer 
+				        	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCym7xb1j_WoYOuD_qLjSQ-HbtxGNCv22A&callback=initMap"></script>';
+				        	echo "<div id='map'></div>";
 				        }
-				        echo "id: " . $row["id"]. "<br>";
 				    }
 				} else {
 				    echo "0 results";
@@ -42,5 +59,9 @@
 			}
 			$conn->close();
 		?>
+		<div id='map'></div>
+
+   		
+		
 	<body>
 </html>
